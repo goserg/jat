d84ea45bc9e9b89f4d0b267888709c108e13ffa7
goserg <gor-serg@bk.ru>
2022-02-19 17:41:29 +0300

    added error handling

--- pkg/colorify/colorjson/colorjson.go
+++ pkg/colorify/colorjson/colorjson.go
@@ -11,7 +11,9 @@ type CJ struct {
 
 func (cj *CJ) Colorify(raw json.RawMessage) (json.RawMessage, error) {
 	var obj map[string]interface{}
-	json.Unmarshal(raw, &obj)
+	if err := json.Unmarshal(raw, &obj); err != nil {
+		return nil, err
+	}
 
 	f := colorjson.NewFormatter()
 	f.Indent = 4
